96ac2e1fdcb14f5fd35cd950595927e1e084e3c6
Cosmin Dumitriu <47316223+cossminuu@users.noreply.github.com>
2020-11-04 20:41:47 +0100

    Update firebase.prod.js

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -4,14 +4,7 @@ import "firebase/auth";
 
 
 const config = {
-    apiKey: "AIzaSyAD7Pc8CxvMWUF1nBYnigI8BlJDaj7U_Bw",
-    authDomain: "netflix-clone-53030.firebaseapp.com",
-    databaseURL: "https://netflix-clone-53030.firebaseio.com",
-    projectId: "netflix-clone-53030",
-    storageBucket: "netflix-clone-53030.appspot.com",
-    messagingSenderId: "468334228041",
-    appId: "1:468334228041:web:208db6ff29a3ad5d594598",
-    measurementId: "G-Z0DJ0TH7WM"
+    //your api
 }
 
 
